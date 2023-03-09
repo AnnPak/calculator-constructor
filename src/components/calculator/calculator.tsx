afc74866263338534221438ca94a3db76fec11e2
AnnPak <pandaqwerty1357@yandex.ru>
2023-03-09 23:34:18 +0300

    show numbers on the display

--- src/components/calculator/calculator.tsx
+++ src/components/calculator/calculator.tsx
@@ -1,15 +1,26 @@
 import styles from "./calculator.module.scss";
 import classnames from "classnames";
 import { OPERATIONS, CALCULATOR_VALUES } from "../../utils/constants";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const Calculator = () => {
-
   const calculatorValuesReverse = !!CALCULATOR_VALUES.length ? [...CALCULATOR_VALUES]?.reverse() : undefined;
+  const [calculatorValue, setCalculatorValue] = useState<string>('');
+  const [currentNumber, setCurrentNumber] = useState<string>('');
+  const [currentOperation, setCurrentOperation] = useState<string|null>(null);
+
+  const clickOnValue = (value:string) => {
+    setCalculatorValue(calculatorValue.concat(value));
+    setCurrentNumber(currentNumber.concat(value))
+  }
+
+  useEffect(() => {
+    console.log(calculatorValue)
+  }, [calculatorValue])
     
     return (
         <>
             <div className={classnames(styles.calculatorWrapper, "p-4")}>
-                <p className={styles.calculatorDisplay}>0</p>
+                <p className={styles.calculatorDisplay}>{calculatorValue ? calculatorValue : 0}</p>
             </div>
             <div className={classnames(styles.calculatorWrapper, styles.operationsBlock, "p-4")}>
                 {Object.entries(OPERATIONS).map(([key, value]) => (
@@ -30,6 +41,7 @@ const Calculator = () => {
                             <button
                                 className={classnames(styles.operationsButton, styles.grayButton)}
                                 key={index}
+                                onClick ={() => clickOnValue(`${value}`)}
                             >
                                 {value}
                             </button>
